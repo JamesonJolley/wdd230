e54c89748e1ca8ef156b12fe0e490c7b0f9b1633
JamesonJolley <jol21004@byui.edu>
2022-03-02 06:31:26 -0700

    card update

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -19,10 +19,12 @@ fetch(requestURL)
   let h2 = document.createElement('h2');
   let portrait = document.createElement('img');
   let profitNumber = document.createElement('p')
+  let dateOfBirth = document.createElement('p')
 
   // Change the textContent property of the h2 element to contain the prophet's full name
   h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  profitNumber.textContent =`The ${prophet.order} prophet; born: ${prophet.birthdate}`
+  profitNumber.textContent =`Prophet number:${prophet.order}`
+  dateOfBirth.textContent =`Date of birth: ${prophet.birthdate}`
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   portrait.setAttribute('src', prophet.imageurl);
   portrait.setAttribute('alt', `Portait of ${prophet.name} - ${prophet.lastname}`);
@@ -32,6 +34,7 @@ fetch(requestURL)
   card.appendChild(h2);
   card.appendChild(portrait);
   card.appendChild(profitNumber)
+  card.appendChild(dateOfBirth)
 
   // Add/append the existing HTML div with the cards class with the section(card)
   cards.appendChild(card);
