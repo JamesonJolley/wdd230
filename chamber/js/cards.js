e447ad3df7fc7aeee87209d93105afc4ce70411d
JamesonJolley <jol21004@byui.edu>
2022-03-05 04:05:12 -0700

    jason stiff

--- chamber/js/cards.js
+++ chamber/js/cards.js
@@ -1,19 +1,26 @@
 const contaner = document.querySelector('#cards')
 
 
-fetch('chamber/data/data.json').then(response => {
-    return response.json();}).then(data => {
-        data.array.forEach(element => {
-            console.log(element)
-        });
-
-    });
+fetch("data/data.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
 
 const makeCard = (data) =>{
     let address = document.createElement("p")
     let phone = document.createElement("p")
     let logo = document.createElement("img")
     let name = document.createElement("p")
+    let card = document.createElement("div")
 
+    address.innerText = data.address
+    phone.innerText = data.phone
+    logo.src = data.logo
+    name.innerHTML = data.name
 
+    card.appendChild(address)
+    card.appendChild(phone)
+    card.appendChild(logo)
+    card.appendChild(name)
+
+    contaner.append(card)
 }
