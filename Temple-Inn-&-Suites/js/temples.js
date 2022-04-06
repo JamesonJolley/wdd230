074a9f067ea785eda262c595c60d5ce641102543
JamesonJolley <jol21004@byui.edu>
2022-04-06 06:55:20 -0700

    stuff

--- Temple-Inn-&-Suites/js/temples.js
+++ Temple-Inn-&-Suites/js/temples.js
@@ -1,3 +1,43 @@
 const contater = document.querySelector('#temples')
+const requestURL ="https://jamesonjolley.github.io/wdd230/Temple-Inn-&-Suites/data/temples.json"
 
-fetch()
+fetch(requestURL).then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject); //temporary checking for valid response and data parsing
+    const cards = jsonObject;
+    
+    cards.forEach(element => {
+      makeCard(element)
+    });
+  });
+
+const makeCard =(element)=>{
+    let card = document.createElement("div")
+    let name = document.createElement('h1')
+    name.innerText = element.name
+    card.appendChild(name)
+
+    let tempimg = document.createElement('img')
+    tempimg.loding = 'lazy'
+    tempimg.src = element.img
+    card.appendChild(tempimg)
+
+    let list = document.createElement('ul')
+    let phone = document.createElement('li')
+    phone.innerText = element.Telephone
+    list.appendChild(phone)
+
+    let addressli = document.createElement('li')
+    let link = document.createElement('a')
+    link.innerText = element.Address
+    link.href = element.link
+    link.target ="_blank"
+    addressli.appendChild(link)
+    list.appendChild(addressli)
+
+    card.appendChild(list)
+    contater.appendChild(card)
+
+}
